3e3131654e73ac93901d61c1c30a50b8239dea08
theLomax <thelomax@gmail.com>
2025-10-09 19:04:25 -0500

    Update js-to-dist.js
    change `path.join()` to `join()`

--- tools/js-to-dist.js
+++ tools/js-to-dist.js
@@ -11,6 +11,6 @@ const outDir = join(__dirname, '..', 'dist', 'js');
 fs.mkdirSync(outDir, { recursive: true });
 const files = fs.readdirSync(srcDir).filter(f => f.endsWith('.js'));
 files.forEach(f => {
-  fs.copyFileSync(path.join(srcDir, f), path.join(outDir, f));
+  fs.copyFileSync(join(srcDir, f), join(outDir, f));
 });
 console.log(`Copied ${files.length} JS files to dist/js`);
